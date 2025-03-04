3c77fe4ea8bd520fea46645612e8f4b416302ec5
SilvioKacorri <s.kacorri@calmcoders.net>
2025-03-04 15:10:40 +0100

    msg

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,26 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
-        //1
-        String[] cars = {"Tesla", "Benz", "Aston Martin", "BMW", "Volkswagen"};
-        Cars car1 = new Cars(cars[0], "Y", 2019, "N/A");
-        Cars car2 = new Cars(cars[1], "C-class", 2013, "6.2");
-        Cars car3 = new Cars(cars[3], "520d", 2017, "2.0");
-
-        List<Cars> cars2 = new ArrayList<>();
-        cars2.add(car1);
-        cars2.add(car2);
-        cars2.add(car3);
-
-        HashMap<String, Cars> carOwners = new HashMap<>();
-        carOwners.put("Silvio", car2);
-        carOwners.put("A", car1);
-        carOwners.put("B", car3);
-        System.out.println(carOwners);
-
-
-        for (int i = cars2.size()-1; i >= 0; i--) {
-            System.out.println(cars2.get(i));
+        List<String> strings = new ArrayList<>();
+        strings.add("Dog");
+        strings.add("Meter");
+        strings.add("Bol");
+        strings.add("Hekur");
+        strings.add("Hop");
+        Map<Integer, ArrayList> stringMap = new HashMap<>();
+        ArrayList<String> tempStrings1 = new ArrayList<>();
+        ArrayList<String> tempStrings2 = new ArrayList<>();
+        for (String tempString : strings) {
+            int intKey = tempString.length();
+            if (!stringMap.containsKey(intKey)) {
+                ArrayList<String> knp = new ArrayList<>();
+                knp.add(tempString);
+                stringMap.put(intKey,knp);
+            } else {
+                stringMap.get(intKey).add(tempString);
+            }
         }
-        cars2.remove(car1);
+        System.out.println(stringMap);
+
     }
 }
